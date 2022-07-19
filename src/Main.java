6e2c67e218766a9a5e0bab5b4392f1de8f590d2c
Hades62442 <83868670+Hades62442@users.noreply.github.com>
2022-07-19 18:54:01 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+    // public - can be accessed from other parts of program
+    // void - returns nothing (void value)
+    // main - function name
+    // String[] args - arguments in the function
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello world!"); // strings are always surrounded by " "
     }
 }
